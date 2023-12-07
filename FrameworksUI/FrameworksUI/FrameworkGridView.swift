5fd121008b169cd5df8534e5610fd32feeabc5d4
Carlos Kimura <ckimura@gmail.com>
2023-12-07 14:56:26 -0300

    project setup

---
+++ FrameworksUI/FrameworksUI/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  FrameworksUI
+//
+//  Created by Carlos Kimura on 07/12/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
---
+++ FrameworksUI/FrameworksUI/FrameworksUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksUIApp.swift
+//  FrameworksUI
+//
+//  Created by Carlos Kimura on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
