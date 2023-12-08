48eb9a7c6384b708a914baa4d0e0010831e43458
Carlos Kimura <ckimura@gmail.com>
2023-12-08 16:22:01 -0300

    added navigation stack

--- FrameworksUI/FrameworksUI/Screens/FrameworkGridView/FrameworkGridViewModel.swift
+++ FrameworksUI/FrameworksUI/Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -9,13 +9,5 @@ import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject {
     
-    var selectedFramework: Framework? {
-        didSet {
-            isShowingDetailView = true
-        }
-    }
-    
-    @Published var isShowingDetailView: Bool = false
-    
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
 }
